a00d33e4b7d9795701c62aa64758b7861c86d2bc
Amit <amitmohammad46@gmail.com>
2026-06-28 00:56:05 +0600

    config the database

---
+++ internal/config/database.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"log"
+	"os"
+	"spotsync/internal/models"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	dsn := os.Getenv("DATABASE_URL")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database", err)
+	}
+	db.AutoMigrate(
+		&models.User{},
+		&models.ParkingZone{},
+		&models.Reservation{}
+	)
+	DB = db
+	log.Println("Database connected successfully", err)
+}
